754ace8c81692db85903f3ca6c867860aa9cbea3
npgeorge <npgeorge@users.noreply.github.com>
2020-01-20 12:59:05 -0500

    added some proff of concept utility files

---
+++ lambdata_npgeorge/__init__.py
@@ -0,0 +1,12 @@
+"""
+lambdata - a collection of data science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+
+#sample code
+
+#put global variable in all caps
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
