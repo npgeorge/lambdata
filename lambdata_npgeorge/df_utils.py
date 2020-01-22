e59f71b3d24a42ec5fdf684982db2b8ec9a412bd
Nick George <53956922+npgeorge@users.noreply.github.com>
2020-01-21 22:08:36 -0500

    added sample dataframe and utility functions

--- lambdata_npgeorge/df_utils.py
+++ lambdata_npgeorge/df_utils.py
@@ -2,6 +2,35 @@
 utility functions for working with DataFrames
 """
 
-import pandas
+import pandas as pd
+import numpy as np
 
-TEST_DF = pandas.DataFrame([1,2,3])
+#from example in class
+#TEST_DF = pandas.DataFrame([1,2,3,np.nan,4])
+
+#made up data
+df = pd.DataFrame({
+    'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'], 
+    'last_name': ['Miller', 'Jacobson', 'Ali', 'Milner', 'Cooze'],
+    'DoB': ['12/20/1991', '05/04/1988', '06/18/1974', '08/29/1980', '11/03/1965'],
+    'siblings': [2,3,1,np.nan,3]
+})
+
+#checking data frames for nulls
+missing = raw_data.isnull().values.any()
+print(missing)
+
+#function to split dates into month, day, and year
+def wrangle(X):
+  
+    #breaking out month
+    X['month'] = X.DoB.astype(str).str[:2].astype(int)
+    #days
+    X['day'] = X.DoB.astype(str).str[3:5].astype(int)
+    #years
+    X['year'] = X.DoB.astype(str).str[6:].astype(int)
+
+    # return the wrangled dataframe
+    return X
+
+df = wrangle(df)
