2e9f822b77e5baaaefa7c9c9650d4a7cb94c3928
Nick George <53956922+npgeorge@users.noreply.github.com>
2020-01-27 00:38:48 -0500

    part 7

---
+++ part7.py
@@ -0,0 +1,19 @@
+### Part 7 - Questions (and your Answers)
+
+Acme Corporation isn't just a few `.py` files. If you want to grow in your
+career here, you'll have to answer the following:
+
+- What, in your opinion, is an important part of code reviews? That is, what is
+  something you pay attention to when you review code, and that you appreciate
+  when others do the same for your code?
+  I pay close attention to the methods used, and whether they are redundant or not.
+  In many cases it is easy to repeat yourself, and I find myself having the same urge.
+  Good code is an iterative effort and should be approached as such.
+- We have an awful lot of computers here, and it gets pretty confusing with
+  slightly different things running on all of them. How could containers help us
+  improve this situation?
+  Containers could help us bin these tests in various ways, as well as help us keep
+  each test relative to its respective attribute. This way, we could run more specific
+  tests for each parameter. 
+
+Answer both of these questions (baseline ~5 sentences) here in text.
